a71ba756919b4e79bdb1916a56240113b71e81ee
unknown <rahulharihar71@gmail.com>
2024-09-16 16:01:24 +0530

    initial commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className="container ">
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>About Us</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+    </div>
+  );
+}
+
+export default NavBar
