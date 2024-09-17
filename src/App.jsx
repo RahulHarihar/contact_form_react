d56170bb6c9b8a452c0562fad8765e5341c337d6
unknown <rahulharihar71@gmail.com>
2024-09-17 21:05:45 +0530

    Updated contactinfo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
-import NavBar from './components/NavBar'
+import ContactInfo from './components/ContactInfo/ContactInfo'
+import NavBar from './components/NavBar/NavBar'
 
 function App() {
   
@@ -7,6 +8,7 @@ function App() {
   return (
     <div>
       <NavBar />
+      <ContactInfo />
     </div>
   )
 }
